884c4415def85db83c7783faf199bdcae6cc09c0
Algo <algorenstudio@gmail.com>
2021-09-23 10:43:05 -0400

    Mostly updated weatherController.js to make evaluateWeatherUrl function do majority of the string analyzing. Made the critical change to to simply remove any trailing forward slashes from all routes. Also added any if statement that handles cityURL routes without a following city name (empty) to send a JSON response object with the reason for error and to proceed with a city name. Cleaned the code up a bit and added some additional comments. Most of the work left now is working with data in weatherModel.js and introducing error handling to certain functions.

--- controller/weatherController.js
+++ controller/weatherController.js
@@ -19,21 +19,35 @@ async function getAllWeather(req, res) {
 // @desc    Gets City Weather Data
 // @route   GET /weather/city
 async function getCityWeather(req, res) {
+    let city_name = ''
     console.log("Inside getCityWeather: \n" + req.url.split('/')[3])
-    try {
-        const cityWeatherData = await weatherData.findCity(city_name = 'apopka')
+    //  Checks that a city name exists.
 
-        res.writeHead(200, { 'Content-Type': 'application/json'})
+    //  Gets city name from URL.
+    city_name = req.url.split('/')[3].toLowerCase().trim()
+
+    try {
+        const cityWeatherData = await weatherData.findCity(city_name)
+        //  Sends data to user
+        res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify(cityWeatherData))
-    } catch (error) {
-        console.log(error)
+    }
+    catch (error) {
+        console.log(error.message)
     }
 }
 
 // @desc    Gets Hour Weather Data
 // @route   GET /weather/hour
 async function getHourWeather(req, res) {
+    try {
+        const hourWeatherData = await weatherData.findHour()
 
+        res.writeHead(200, { 'Content-Type': 'application/json'})
+        res.end(JSON.stringify(hourWeatherData))
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
@@ -44,23 +58,44 @@ async function evaluateWeatherURL(req, res) {
         //  saying sorry we have no data for that city or time frame.
         //  If the url sent is in a invalid form call getInstructions.
         //  Else return there was a technical error.
+        const hourURL = new UrlPattern('/weather/hour')
         const cityURL = new UrlPattern('/weather/city(/:anything)')
-        const cityTrailingURL = new UrlPattern('/weather/city(/:anything/)')
+        const cityWithNoNameURL = new UrlPattern('/weather/city')
         const allURL = new UrlPattern('/weather/all')
-        const allTrailingURL = new UrlPattern('/weather/all/')
 
         console.log("Inside evaluateWeatherURL: \n" + req.url)
+        console.log("Inside evaluateWeatherURL: \n" + req.url.toString().length)
+        let urlString = req.url.toString().trim()
+        //  Removes last forward slash if it is the last character of the url string
+        if (urlString.lastIndexOf('/') == urlString.length - 1) {
+            urlString = urlString.slice(0, urlString.length - 1)
+        }
 
+        //  Matching urlString to appropriate 'get' function
+        if (hourURL.match(urlString)) {
+            console.log("Matched for hourURL!")
+            getHourWeather(req, res)
+        }
 
-        if (cityURL.match(req.url) || cityTrailingURL.match(req.url)) {
+        if (cityURL.match(urlString)) {
             //  Get weather data for a specific city: getCityWeather()
             console.log("Matched for cityURL!")
             getCityWeather(req, res)
         } 
 
-        if (allURL.match(req.url) || allTrailingURL.match(req.url)) {
+        if (allURL.match(urlString)) {
             console.log("Matched for allURL!")
             getAllWeather(req, res)
+        }
+
+        if (cityWithNoNameURL.match(urlString)) {
+            //  Sends 400 error code and JSON object with error message.
+            //  To Do: Create an errors.json document that holds error messages
+            //  like this in JSON format.
+            const noCityNameErrorMessage =
+                "Error most likely due to no city following '/city/'. Please try again with a city name."
+            res.writeHead(400, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ "message": noCityNameErrorMessage }))
         }
 
         getMainRouteInstructions(req, res)
--- model/weatherModel.js
+++ model/weatherModel.js
@@ -15,13 +15,31 @@ function findCity(city_name) {
         console.log("Inside findCity, weatherData: \n" + city_name)
 
         let cityObjects = weatherData.filter(object => object.location.city.name.toLowerCase() == city_name)
-        console.log(cityObjects.toString())
+
+        console.log(cityObjects.length)
+        if (cityObjects.length == 0) {
+            reject("Could not find any weather data for this city.")
+        }
 
         resolve(cityObjects)
+
+    })
+}
+
+function findHour() {
+
+    return new Promise((resolve, reject) => {
+        let hourObjects = weatherData.filter(object => object.time.from.split(':')[0] == 19)
+
+        console.log(hourObjects)
+        console.log(hourObjects.length)
+
+        resolve("Find hour working.")
     })
 }
 
 module.exports = {
     findAll,
-    findCity
+    findCity,
+    findHour
 }
